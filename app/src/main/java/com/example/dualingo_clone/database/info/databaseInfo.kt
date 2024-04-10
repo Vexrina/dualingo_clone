41c86784c33b02c9d4c9964b28f0b6497f5e33e5
Konstantin Schavlev <76181740+Vexrina@users.noreply.github.com>
2024-04-10 19:26:59 +0300

    delete secret

--- app/src/main/java/com/example/dualingo_clone/database/info/databaseInfo.kt
+++
@@ -1,5 +0,0 @@
-package com.example.dualingo_clone.database.info
-
-const val url = "https://umtzxyrfsvufvihqziij.supabase.co"
-//const val key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtdHp4eXJmc3Z1ZnZpaHF6aWlqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTE1MzA0ODQsImV4cCI6MjAyNzEwNjQ4NH0.HgzczBU5_iCGenOipm1MaXagSDKFnzmBQ_C4rOMEfwA"
-const val key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtdHp4eXJmc3Z1ZnZpaHF6aWlqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMTUzMDQ4NCwiZXhwIjoyMDI3MTA2NDg0fQ.en3WunatMD0b1G2k2JjlNAcRIQYC-obQJ2IIg6qQdzg"
