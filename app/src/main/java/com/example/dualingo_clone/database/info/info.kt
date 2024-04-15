c57919003024fbc965e5d4f59e3876858a6022be
Konstantin Schavlev <76181740+Vexrina@users.noreply.github.com>
2024-04-15 23:02:10 +0300

    Delete app/src/main/java/com/example/dualingo_clone/database/info directory

--- app/src/main/java/com/example/dualingo_clone/database/info/info.kt
+++
@@ -1,4 +0,0 @@
-package com.example.dualingo_clone.database.info
-
-val url = "https://umtzxyrfsvufvihqziij.supabase.co"
-val key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtdHp4eXJmc3Z1ZnZpaHF6aWlqIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcxMTUzMDQ4NCwiZXhwIjoyMDI3MTA2NDg0fQ.en3WunatMD0b1G2k2JjlNAcRIQYC-obQJ2IIg6qQdzg"
